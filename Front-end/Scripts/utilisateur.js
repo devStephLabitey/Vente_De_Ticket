53589aa3bc0073ff9f9a8f0229b5cb6e03355f71
steph <labiteydevsteph@gmail.com>
2024-01-21 20:54:42 +0100

    New commit

---
+++ Front-end/Scripts/utilisateur.js
@@ -0,0 +1,7 @@
+import { Tab, initMDB } from "mdb-ui-kit";
+
+initMDB({ Tab });
+const changeProfil= document.querySelector(".profil");
+changeProfil.addEventListener("click", ()=>{
+    alert("a")
+})
